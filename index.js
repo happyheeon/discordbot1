24456f31b6102f6fa9a8beb6066d3339c127052b
happyheeon <157951953+happyheeon@users.noreply.github.com>
2025-02-17 15:47:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import dotenv from 'dotenv'
-dotenv.config()
+require(dotenv).config()
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
