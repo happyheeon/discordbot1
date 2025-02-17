9bad8498d2ec7faede4bda1a37dc0e63764ad6ca
happyheeon <157951953+happyheeon@users.noreply.github.com>
2025-02-17 16:21:04 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
