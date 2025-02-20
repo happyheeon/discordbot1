f62c9a90953a58480a6fa3a0a9e5744011ae6189
happyheeon <157951953+happyheeon@users.noreply.github.com>
2025-02-20 16:59:45 +0900

    Create update.js

---
+++ commands/update.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder} = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder().setName("업데이트알림").setDescription("업뎃알립!").addStringOption(option =>
+            option.setName('message')
+                .setDescription('보낼 메시지')
+                .setRequired(true)
+        ),
+  async execute(interaction) {
+    
+    client.channels.cache.get(1341367065667436616).send(interaction.options.getString('message') + `\n@`)
+  },
+};
